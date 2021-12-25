b09bb03b187f8c22271f4313652775f8febc9d6c
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2021-12-25 14:53:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# PFIM: Personal Finance Manager
+**PFIM** is a basic command-line tool for track personal finance. It let you
+record and track what you spend or earn, and it facilitates viewing reports of
+your entries base on criteria and ability to provides a nice summary.
+
+## Overview
+
+## Features
+
+## Examples
---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+with open("README.md", "r") as fd:
+    LONG_DESCRCIPTION = fd.read()
+
+setup(
+    name="pfim",
+    author="Eyram K. Apetcho",
+    author_email="orion2dev@gmail.com",
+    url="",
+    python_requires=">=3.6",
+    license="BSD-2",
+    description="Personal Finance Manager",
+    long_description=LONG_DESCRCIPTION,
+    long_description_content_type="text/markdown",
+    platforms=["Windows", "Linux", "Mac OS X"],
+    classifiers=[
+        "Development Status:: Pre-Alpha",
+        "Intended Audience :: Customer Service",
+        "Intended Audience :: Other Audience",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Finance",],
+    keywords=["Personal finance manager", "command-line application"],
+    tests_require=["pytest"],
+    package_dir={"pfim": "pfim"},
+    packages=["pfim"],
+    )
