4c780b98cdc1805ecdb9acdb34919288c742e16a
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2021-12-26 01:20:34 +0100

    Add update commands handling to the cmd-line parser

--- pfim/_cmd_parser.py
+++ pfim/_cmd_parser.py
@@ -28,74 +28,81 @@ The sub-commands options are describe below.
         dest="fancy", help="Toggle into fancy output mode")
 
     # group1
-    group1 = parser.add_argument_group(
-        "Add a new amount received or spent to the database")
-    group1.add_argument("--tag", nargs="?", type=str, dest="atag",
+    addgrp = parser.add_argument_group(
+        "Record adding commands and options")
+    addgrp.add_argument("--tag", nargs="?", type=str, dest="atag",
         help=("Attach a tag to this received. Default is RCV if money "  
             "received"), default="RCV", metavar="TAG")
-    group1.add_argument("--date", nargs="?", type=str, dest="aedate",
+    addgrp.add_argument("--date", nargs="?", type=str, dest="aedate",
         help="Add the specific date for the received amount",
         default="current date", metavar="YYYY-MM-DD")
-    addex = group1.add_mutually_exclusive_group()
+    addex = addgrp.add_mutually_exclusive_group()
     addex.add_argument("-rcv", type=float, dest="rcv",
         help="The actual amount you received", metavar="AMOUNT")
     addex.add_argument("-spent", type=float, dest="aspent",
         help="The actual amount you spent", metavar="AMOUNT")
 
-    # group2
-    # group3
-    group2 = parser.add_argument_group("Show report")
-    group2.add_argument("--sort-date", action="store_true", dest="sortDate",
+    # showcmds=(show|show-rcv|show-spent)
+    # showopts=[sort-(date|tag|amount)]|[after-date|before-date|on-date|--desc]
+    showgrp = parser.add_argument_group("Report showing commands and options")
+    showgrp.add_argument("--sort-date", action="store_true", dest="sortDate",
         help="Show output ordered by date")
-    group2.add_argument("--sort-tag", action="store_true", dest="sortTag",
+    showgrp.add_argument("--sort-tag", action="store_true", dest="sortTag",
         help="Show output ordered by tag")
-    group2.add_argument("--sort-amount", action="store_true", dest="sortAmount",
+    showgrp.add_argument("--sort-amount", action="store_true", dest="sortAmount",
         help="Show output ordered by amount value")
-    group2.add_argument("--desc", action="store_true", dest="sortDesc",
+    showgrp.add_argument("--desc", action="store_true", dest="sortDesc",
         help="Sort in descending order. The default sorting order is ascending")
-    group2.add_argument("--before-date", type=str, dest="beforeDate",
+    showgrp.add_argument("--before-date", type=str, dest="beforeDate",
         help="Show records before a specific date",
         metavar="YYYY-MM-DD")
-    group2.add_argument("--after-date", type=str, dest="afterDate",
+    showgrp.add_argument("--after-date", type=str, dest="afterDate",
         help="Show records after a specific date",
         metavar="YYYY-MM-DD")
-    group2.add_argument("--on-date", type=str, dest="onDate",
+    showgrp.add_argument("--on-date", type=str, dest="onDate",
         help="Show record on a specific date",
         metavar="YYYY-MM-DD")
-    showex = group2.add_mutually_exclusive_group()
+    showex = showgrp.add_mutually_exclusive_group()
     showex.add_argument("-show", action="store_true", dest="show",
         help="Show all records in the database")
     showex.add_argument("-show-recv", action="store_true", dest="showRcv",
         help="Show all records in the database corresponding to incomes")
     showex.add_argument("-show-spent", action="store_true", dest="showSpent",
         help="Show all records in the database corresponting to expenses")
-    # cmds = (show|show-rcv|show-spent)
-    # opts = [sort-(date|tag|amount)]|[after-date|before-date|on-date|--desc]
+    
     # group4
-    # 15. pfim --show-earned
-    # 16. pfim --show-earned --sort-date
-    # 17. pfim --show-earned --sort-tag
-    # 18. pfim --show-earned --sort-amount
-    # 19. pfim --show-earned --before-date=YYYY-MM-DD [--sort-date|--sort-date|--sort-amount]
-    # 20. pfim --show-earned --after-date=YYYY-MM-DD [--sort-date|--sort-date|--sort-amount]
     # group5
-    # 21. pfim --show-spent
-    # 22. pfim --show-spent --sort-date
-    # 23. pfim --show-spent --sort-tag
-    # 24. pfim --show-spent --sort-amount
-    # 24. pfim --show-spent --before-date=YYYY-MM-DD [--sort-date|--sort-date|--sort-amount]
-    # 25. pfim --show-spent --after-date=YYYY-MM-DD [--sort-date|--sort-date|--sort-amount]
     # group6
-    # 26. pfim --update --tag=TAG1 --new-tag=TAG2
-    # 27. pfim --update --date=YYYY-MM-DD
-    # 28. pfim --update --earned --amount=VALUE1 --new-amount=VALUE2
-    # 29. pfim --update --spent --amount=VALUE2 --new-amount=VALUE2
+    updategrp = parser.add_argument_group("Update record commands and options")
+    # upcmds = (update|update-rcv|update-spent)
+    # upOpts = [(old-tag, new-tag)|(old-date,new-date)|(old-amount,new-amount)]
+    updategrp.add_argument("--old-tag", type=str, dest="oldTag", metavar="TAG1",
+        help="Previous tag to be updated")
+    updategrp.add_argument("--new-tag", type=str, dest="newTag", metavar="TAG2",
+        help="New tag to update the old one")
+    updategrp.add_argument("--old-date", type=str, dest="oldDate", 
+        metavar="YYYY-MM-DD", help="Previous date to be updated")
+    updategrp.add_argument("--new-date", type=str, dest="newDate", 
+        metavar="YYYY-MM-DD", help="New date to update the old one")
+    updategrp.add_argument("--old-amount", type=float, dest="oldAmount", 
+        metavar="VALUE1",
+        help="Previous amount value to be updated")
+    updategrp.add_argument("--new-amount", type=float, dest="newAmount", 
+        metavar="VALUE2", help="New amount value to update the old one")
+    updatex = updategrp.add_mutually_exclusive_group()
+    updatex.add_argument("-update", action="store_true", default="update",
+        help="Update record(s)")
+    updatex.add_argument("-update-rcv", action="store_true", dest="updateRcv",
+        help="Update record(s) corresponding to income")
+    updatex.add_argument("-update-spent", action="store_true", 
+        dest="updateSpent", help="Update record(s) corresponding to expense")
+    
     # group7
-    # 30. pfim --delete-all
-    # 31. pfim --delete --tag=TAG1
-    # 32. pfim --delete --date=YYYY-MM-DD
-    # 33. pfim --delete --earned --amount=VALUE
-    # 34. pfim --deltte --spent --amount=VALUE
+    # 30. pfim -delete-all
+    # 31. pfim -delete --target-tag=TAG1
+    # 32. pfim -delete --target-date=YYYY-MM-DD
+    # 33. pfim -delete-rcv --target-amount=VALUE
+    # 34. pfim -deltte-spent --target-amount=VALUE
 
 
     return parser
