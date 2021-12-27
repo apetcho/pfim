ee9084916913a29ab5bd14ee37ef456465fc439e
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2021-12-27 17:03:28 +0100

    Refine pfim *delete* subcommand processing

--- pfim/_cmd_parser.py
+++ pfim/_cmd_parser.py
@@ -37,10 +37,8 @@ def _cmd_parser():
     # -- record subcommand
     recparser = subparsers.add_parser(
         name="record",
-        help="Record an income or expense entry",
-        usage=(
-            "pfim record [--date=YYYY-MM-DD][--tag=TAG][--descr=TEX] "
-            "(--exp=VALUE | --inc=VALUE)"))
+        usage="\n\tpfim record [OPTIONS]",
+        help="Record an income or expense entry")
     recparser.add_argument("--date", type=str, dest="recdate",
         metavar="YYYY-MM-DD", default=date.today().isoformat(),
         help="Specify the date for this record. Default is current date")
@@ -60,6 +58,7 @@ def _cmd_parser():
     # -- report subcommand
     repparser = subparsers.add_parser(
         name="report",
+        usage="\n\tpfim report [OPTIONS]",
         help="Show report for a given query")
     repparser.add_argument("--sort-date", action="store_true",
         dest="sortDate",
@@ -88,8 +87,6 @@ def _cmd_parser():
     repparser.add_argument("--all", action="store_true", dest="allQuery",
         help="Show report for all records")
     
-
-
     # -- update subcommand parser
     # upcmds = (update|update-rcv|update-spent)
     # upOpts = [(old-tag, new-tag)|(old-date,new-date)|(old-amount,
@@ -121,108 +118,31 @@ The format is: --desc YYYY-MM-DD OLD_DESCR NEW_DESCR
 \tExample:  2022-02-17 'Concert reservation' 'Hotel booking for PyCon'"""))
 
     # -- delete subcommand parser
-    delparser = subparsers.add_parser(
-        name="delete",
-        help="Delete record(s) for given query")
-
-    # recparser.add_argument("--tag", type=str, dest="extag", metavar="TAG",
-    #     help="Tag a record")
-    # recparser.add_argument("-descr", "--description", type=str)
-    # recex = recparser.add_mutually_exclusive_group()
-    # recex.add_argument("--expense", type=float, dest="expense", metavar="VALUE", help="Record the expense of thie VALUE")
-
-    #parser.add_argument("--fancy-output", action="store_true",
-    #    dest="fancy", help="Toggle PFIM into fancy output mode")
-
-    # addgrp
-    # addcmds = (rec-rcv|rec-xpx)
-    # addOpts = [date|tag]
-    # addgrp = parser.add_argument_group(
-    #     "Record income/expense commands and options")
-    # addgrp.add_argument("--tag", nargs="?", type=str, dest="recTag",
-    #     help=("Attach a tag to this received. Possible value are RCV and "  
-    #         "XPX. [default: RCV]"), default="RCV", metavar="TAG",
-    #         choices=["RCV", "XPX"])
-    # addgrp.add_argument("--date", nargs="?", type=str, dest="recDate",
-    #     help="Add the specific date for the received amount",
-    #     default="current date", metavar="YYYY-MM-DD")
-    # addgrp.add_argument("--description", type=str, dest="Description",
-    #     help='Short description for a new record. [default: "N/A"]',
-    #     default="N/A", metavar="TEXT")
-    # addex = addgrp.add_mutually_exclusive_group()
-    # addex.add_argument("-record-rcv", type=float, dest="rcv",
-    #     help="The actual amount you received", metavar="VALUE")
-    # addex.add_argument("-record-xpx", type=float, dest="xpx",
-    #     help="The actual amount you spent", metavar="VALUE")
-
-    # # showcmds=(show|show-rcv|show-spent)
-    # # showopts=[sort-(date|tag|amount)]|[after-date|before-date|on-date|--desc]
-    # showgrp = parser.add_argument_group("Report showing commands and options")
-    # showgrp.add_argument("--sort-date", action="store_true", dest="sortDate",
-    #     help="Show output ordered by date")
-    # showgrp.add_argument("--sort-tag", action="store_true", dest="sortTag",
-    #     help="Show output ordered by tag")
-    # showgrp.add_argument("--sort-val", action="store_true", dest="sortVal",
-    #     help="Show output ordered by income/expense value")
-    # showgrp.add_argument("--desc", action="store_true", dest="sortDesc",
-    #     help="Sort in descending order. The default sorting order is ascending")
-    # showgrp.add_argument("--before-date", type=str, dest="beforeDate",
-    #     help="Show records before a specific date",
-    #     metavar="YYYY-MM-DD")
-    # showgrp.add_argument("--after-date", type=str, dest="afterDate",
-    #     help="Show records after a specific date",
-    #     metavar="YYYY-MM-DD")
-    # showgrp.add_argument("--on-date", type=str, dest="onDate",
-    #     help="Show record on a specific date",
-    #     metavar="YYYY-MM-DD")
-    # showex = showgrp.add_mutually_exclusive_group()
-    # showex.add_argument("-show", action="store_true", dest="show",
-    #     help="Show all records in the database")
-    # showex.add_argument("-show-recv", action="store_true", dest="showRCV",
-    #     help="Show all records in the database corresponding to incomes")
-    # showex.add_argument("-show-xpx", action="store_true", dest="showXPX",
-    #     help="Show all records in the database corresponting to expenses")
-    
-    # updategrp = parser.add_argument_group("Update record commands and options")
-    # # upcmds = (update|update-rcv|update-spent)
-    # # upOpts = [(old-tag, new-tag)|(old-date,new-date)|(old-amount,new-amount)]
-    # updategrp.add_argument("--old-tag", type=str, dest="oldTag", metavar="TAG1",
-    #     help="Previous tag to be updated")
-    # updategrp.add_argument("--new-tag", type=str, dest="newTag", metavar="TAG2",
-    #     help="New tag to update the old one")
-    # updategrp.add_argument("--old-date", type=str, dest="oldDate", 
-    #     metavar="YYYY-MM-DD", help="Previous date to be updated")
-    # updategrp.add_argument("--new-date", type=str, dest="newDate", 
-    #     metavar="YYYY-MM-DD", help="New date to update the old one")
-    # updategrp.add_argument("--old-val", type=float, dest="oldVal", 
-    #     metavar="VALUE1",
-    #     help="Previous amount value to be updated")
-    # updategrp.add_argument("--new-val", type=float, dest="newVal", 
-    #     metavar="VALUE2", help="New amount value to update the old one")
-    # updatex = updategrp.add_mutually_exclusive_group()
-    # updatex.add_argument("-update", action="store_true", default="update",
-    #     help="Update record(s)")
-    # updatex.add_argument("-update-rcv", action="store_true", dest="updateRCV",
-    #     help="Update record(s) corresponding to income")
-    # updatex.add_argument("-update-xpx", action="store_true", 
-    #     dest="updateXPX", help="Update record(s) corresponding to expense")
-    
     # # delcmds = (delete|delete-rcv|delete-spent)
     # # delOpts = [target-tag|target-date|target-mount]
-    # rmgrp = parser.add_argument_group("Delete commands and options")
-    # rmgrp.add_argument("--target-tag", type=str, dest="targetTag",
-    #     metavar="TAG", help="Delete record(s) for a specific tag")
-    # rmgrp.add_argument("--target-date", type=str, dest="targetDate",
-    #     metavar="YYYY-MM-DD", help="Delete record(s) for a specific date")
-    # rmgrp.add_argument("--target-val", type=float, dest="targetVal",
-    #     metavar="VALUE",
-    #     help="Delete record(s) for a specific income/expense value")
-    # rmex = rmgrp.add_mutually_exclusive_group()
-    # rmex.add_argument("-delete", action="store_true", dest="delete",
-    #     help="Delete record(s)")
-    # rmex.add_argument("-delete-rcv", action="store_true", dest="deleteRCV",
-    #     help="Delete record(s) corresponding to incomes")
-    # rmex.add_argument("-delete-xpx", action="store_true", 
-    #     dest="deleteXPX", help="Delete record(s) corresponding to expenses")
-
+    rmparser = subparsers.add_parser(
+        name="delete",
+        usage="\n\tpfim delete [OPTIONS]",
+        help="Delete record(s) for given query")
+    rmparser.add_argument("--exp", type=float, dest="rmExpense",
+        metavar="VALUE",
+        help="Delete record(s) where expense is VALUE")
+    rmparser.add_argument("--inc", type=float, dest="rmIncome",
+        metavar="VALUE",
+        help="Delete record(s) where income is VALUE")
+    rmparser.add_argument("--tag", type=str, dest="rmTag",
+        metavar="TAG",
+        help="Delete record where tag is TAG")
+    rmparser.add_argument("--on", type=str, dest="rmDate",
+        metavar="YYYY-MM-DD",
+        help="Delete record where date is the date provided in YYYY-MM-DD format")
+    rmparser.add_argument("--before", type=str, dest="rmBDate",
+        metavar="YYYY-MM-DD",
+        help="Delete all record(s) before a given date in YYYY-MM-DD format")
+    rmparser.add_argument("--after", type=str, dest="rmADate",
+        metavar="YYYY-MM-DD",
+        help="Delete all records(s) after a given date in YYYY-MM-DD format")
+    rmparser.add_argument("--all", action="store_true", dest="rmAll",
+        help="Delete all records")
+    
     return parser
